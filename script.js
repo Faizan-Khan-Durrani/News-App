d35a70bdafcb82a1fc5072aacd8d1e03a9e78a37
Faizan-Khan-Durrani <faizankhandurrrani@gmail.com>
2025-02-11 23:51:23 +0500

    add search and filter

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const news = document.getElementById("news");
+const search = document.getElementById("search");
+let filters = document.querySelectorAll("ul li button");
 
 let word = "virat kohli";
+search.addEventListener("click", (e) => {
+  word = search.previousElementSibling.value;
+  console.log(word);
+  news.innerHTML = ""; // Clear existing news before fetching new ones
+  FetchNews();
+});
+
+filters.forEach((e) => {
+  e.addEventListener("click", () => {
+    word = e.textContent;
+    console.log(word);
+    news.innerHTML = ""; // Clear existing news before fetching new ones
+    FetchNews();
+  });
+});
 
 async function FetchNews() {
   try {
     const fetched = fetch(
-      `ttps://newsapi.org/v2/everything?q=${word}&from=2025-02-1&sortBy=publishedAt&apiKey=4fbe933697c442e8926000397a4490f8`
+      `https://newsapi.org/v2/everything?q=${word}&from=2025-02-1&sortBy=publishedAt&apiKey=4fbe933697c442e8926000397a4490f8`
     );
     const response = await fetched;
     const data = await response.json();
@@ -16,11 +33,6 @@ async function FetchNews() {
 }
 FetchNews();
 function parseHTML(data) {
-  console.log(data);
-  console.log(data.articles[0].description);
-  console.log("hello", data.articles[0].title);
-  console.log(data.articles[0].url);
-  console.log(data.articles[0].urlToImage);
   for (let i = 0; i <= 5; i++) {
     const card = document.createElement("div");
     card.setAttribute("class", card);
@@ -31,7 +43,7 @@ function parseHTML(data) {
           <img src="${data.articles[i].urlToImage}" alt="" />
         </div>
         <div class="details">
-          <h4 id="heading">${data.articles[i].title}</h4>
+          <h4 id="heading">${String(data.articles[i].title).slice(0, 30)}..</h4>
           <p id="desc">
         ${String(data.articles[i].description).slice(0, 90)}......
           </p>
